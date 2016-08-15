4ca96948d430cf719ed4e91e1a878bd961a2df43
mixa4634 <mixa4634@gmail.com>
2016-08-15 05:04:34 +0300

    Новая текстура фона и динамическое расширение.

--- background.h
+++ background.h
@@ -8,10 +8,12 @@ class background : public object
 {
 public:
 	AUX_RGBImageRec *texture1;
+	double size;
 	background()
 	{
 	type = BACKGROUND;
-	load_texture("images/space.bmp");
+	load_texture("images/space 3 compressed.bmp");
+	size = 150;
 	}
 	virtual void load_texture(LPCSTR name)
 	{
@@ -27,57 +29,69 @@ public:
 		glBindTexture(GL_TEXTURE_2D, texture[0]);
 		glTexImage2D(GL_TEXTURE_2D, 0, 3, texture1->sizeX, texture1->sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE, texture1->data);
 
+		for (unsigned int i = 0; i < stack.size(); i++) //Расширение фона при подлете спутника к нему
+			if (stack[i]->type = SATELLITE)
+				if (((satellite*)stack[i])->r.b.x > size - 20 ||
+					((satellite*)stack[i])->r.b.y > size - 20 ||
+					((satellite*)stack[i])->r.b.z > size - 20)
+					size += 0.5;
+
 		glColor3f(1, 1, 1);
 		glBegin(GL_QUADS);
 
-		glTexCoord2f(0.0, 0.0);
-		glVertex3f(150, 150, 150);
-		glTexCoord2f(1.0, 0.0);
-		glVertex3f(150, 150, -150);
-		glTexCoord2f(1.0, 1.0);
-		glVertex3f(150, -150, -150);
-		glTexCoord2f(0.0, 1.0);
-		glVertex3f(150, -150, 150);
-		glTexCoord2f(0.0, 0.0);
-		glVertex3f(150, 150, 150);
-		glTexCoord2f(1.0, 0.0);
-		glVertex3f(150, -150, 150);
-		glTexCoord2f(1.0, 1.0);
-		glVertex3f(-150, -150, 150);
-		glTexCoord2f(0.0, 1.0);
-		glVertex3f(-150, 150, 150);
-		glTexCoord2f(0.0, 0.0);
-		glVertex3f(150, 150, -150);
-		glTexCoord2f(1.0, 0.0);
-		glVertex3f(150, -150, -150);
-		glTexCoord2f(1.0, 1.0);
-		glVertex3f(-150, -150, -150);
-		glTexCoord2f(0.0, 1.0);
-		glVertex3f(-150, 150, -150);
-		glTexCoord2f(0.0, 0.0);
-		glVertex3f(150, -150, 150);
-		glTexCoord2f(1.0, 0.0);
-		glVertex3f(150, -150, -150);
-		glTexCoord2f(1.0, 1.0);
-		glVertex3f(-150, -150, -150);
-		glTexCoord2f(0.0, 1.0);
-		glVertex3f(-150, -150, 150);
-		glTexCoord2f(0.0, 0.0);
-		glVertex3f(150, 150, 150);
-		glTexCoord2f(1.0, 0.0);
-		glVertex3f(150, 150, -150);
-		glTexCoord2f(1.0, 1.0);
-		glVertex3f(-150, 150, -150);
-		glTexCoord2f(0.0, 1.0);
-		glVertex3f(-150, 150, 150);
-		glTexCoord2f(0.0, 0.0);
-		glVertex3f(-150, 150, 150);
-		glTexCoord2f(1.0, 0.0);
-		glVertex3f(-150, -150, 150);
-		glTexCoord2f(1.0, 1.0);
-		glVertex3f(-150, -150, -150);
-		glTexCoord2f(0.0, 1.0);
-		glVertex3f(-150, 150, -150);
+		glTexCoord2f(1.0 / 2, 2.0 / 3);
+		glVertex3f(size, size, size);
+		glTexCoord2f(3.0 / 4, 2.0 / 3);
+		glVertex3f(size, size, -size);
+		glTexCoord2f(3.0 / 4, 1.0 / 3);
+		glVertex3f(size, -size, -size);
+		glTexCoord2f(1.0 / 2, 1.0 / 3);
+		glVertex3f(size, -size, size);
+
+		glTexCoord2f(1.0 / 2, 2.0 / 3);
+		glVertex3f(size, size, size);
+		glTexCoord2f(1.0 / 2, 1.0 / 3);
+		glVertex3f(size, -size, size);
+		glTexCoord2f(1.0 / 4, 1.0 / 3);
+		glVertex3f(-size, -size, size);
+		glTexCoord2f(1.0 / 4, 2.0 / 3);
+		glVertex3f(-size, size, size);
+
+		glTexCoord2f(3.0 / 4, 2.0 / 3);
+		glVertex3f(size, size, -size);
+		glTexCoord2f(3.0 / 4, 1.0 / 3);
+		glVertex3f(size, -size, -size);
+		glTexCoord2f(1.0, 1.0 / 3);
+		glVertex3f(-size, -size, -size);
+		glTexCoord2f(1.0, 2.0 / 3);
+		glVertex3f(-size, size, -size);
+
+		glTexCoord2f(1.0 / 2, 1.0 / 3);
+		glVertex3f(size, -size, size);
+		glTexCoord2f(1.0 / 2, 0.0);
+		glVertex3f(size, -size, -size);
+		glTexCoord2f(1.0 / 4, 0.0);
+		glVertex3f(-size, -size, -size);
+		glTexCoord2f(1.0 / 4, 1.0 / 3);
+		glVertex3f(-size, -size, size);
+
+		glTexCoord2f(1.0 / 2, 2.0 / 3);
+		glVertex3f(size, size, size);
+		glTexCoord2f(1.0 / 2, 1.0);
+		glVertex3f(size, size, -size);
+		glTexCoord2f(1.0 / 4, 1.0);
+		glVertex3f(-size, size, -size);
+		glTexCoord2f(1.0 / 4, 2.0 / 3);
+		glVertex3f(-size, size, size);
+
+		glTexCoord2f(1.0 / 4, 2.0 / 3);
+		glVertex3f(-size, size, size);
+		glTexCoord2f(1.0 / 4, 1.0 / 3);
+		glVertex3f(-size, -size, size);
+		glTexCoord2f(0.0, 1.0 / 3);
+		glVertex3f(-size, -size, -size);
+		glTexCoord2f(0.0, 2.0 / 3);
+		glVertex3f(-size, size, -size);
 		glEnd();	
 	}
 	
